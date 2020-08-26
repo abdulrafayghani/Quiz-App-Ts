0e65b993bba8425867b4e3cbedc3275392014366
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-08-26 17:16:28 +0500

    Pwa's Functional

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,31 @@
-let CACHE_NAME = 'website';
+// let CACHE_NAME = 'website';
 
-let urlsToCache = [
-  '/static/js/bundle.js',
-  '/static/js/main.chunk.js',
-  '/static/js/1.chunk.js',
-  'inex.html',
+// let urlsToCache = [
+//   '/static/js/bundle.js',
+//   '/static/js/main.chunk.js',
+//   '/static/js/1.chunk.js',
+//   'inex.html',
 
-  'https://opentdb.com/api_category.php',
-  '/',
-];
+//   'https://opentdb.com/api_category.php',
+//   '/',
+// ];
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+// self.addEventListener('install', function (event) {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       console.log('Opened cache');
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       if (response) {
+//         return response;
+//       }
+//       return fetch(event.request);
+//     })
+//   );
+// });
